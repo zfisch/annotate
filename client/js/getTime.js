ceb3b18eac97f4a03daa8882aeeb85adaae75c78
brantchoate <brantchoate@gmail.com>
2015-05-05 12:33:59 -0700

    combining the stuff

--- client/js/getTime.js
+++ client/js/getTime.js
@@ -1,28 +1,3 @@
-//Get video for player.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: obj.video.id,
-    events: {
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-//Listener for player state changes. Calls getTime every second.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    getTime();
-  } else if (event.data = YT.PlayerState.ENDED){
-    //TODO: add handling for player paused.
-    done = true;
-    clearInterval(getTime);
-  }
-}
-
 //This function gets the time of the video on a regular interval, rounds it down to the nearest second.
 var currentVideoTime = 0;
 var getTime = setInterval(function(){
@@ -31,18 +6,6 @@ var getTime = setInterval(function(){
     append(obj.video.annotations[currentVideoTime]);
   }
 }, 1000);
-
-//Hardcorded video json object for testing.
-var obj = {video: {
-  id: 'NCKTeRW3OhQ',
-  name: 'Spoon Guitar',
-  annotations: {
-    13: "<div>SIIIIIICK</div>",
-    27: "<div>FUCK YEA</div>",
-    67: "<div>alright, man! sweet spoon</div>",
-  }
-}}
-
 
 
 
