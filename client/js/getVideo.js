0cd6c50cdcd6e0f821c93d5df31fba889dc52532
Zack Fischmann <zacharyrobert@gmail.com>
2015-05-05 15:34:37 -0700

    initialize annotation html structure and displayvideo logic

---
+++ client/js/getVideo.js
@@ -0,0 +1,24 @@
+//add YOUTUBE api tag to page async
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//add video to page from url
+var displayVideo = function(url){
+  var id = getVideoId(url);
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: id,
+    });
+  }
+}
+
+//get video id from url
+var getVideoId = function(url){
+  var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  return (url.match(p)) ? RegExp.$1 : false ;
+}
