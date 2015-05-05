6b62486ab13a52b5e2d8c08a7db12e7111aa8aae
John Nguyen <hal@Johns-MacBook-Air.local>
2015-05-05 12:04:57 -0700

    finished initial server + db scaffold

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var api = require('./api');
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+app.get('/api/annotators/:annotatorId/media/:mediaId', api.showMedia);
+
+var server = app.listen(9000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+exports = module.exports = app;
